459f20113d60ad0a820c0374670706ab5a15ff72
leegi010605 <91926365+leegi010605@users.noreply.github.com>
2022-04-05 21:37:08 +0900

    changed cal.sh

--- cal.sh
+++ cal.sh
@@ -1,48 +1,41 @@
-#!/bin/bash
-
-num1=$(<num1.txt)
-num2=$(<num2.txt)
-
 if [ $# -le 0 ]; then
-	echo "...none operator paraeter..."
-	echo "1.add"
-	echo "2.sub"
-	echo "3.div"
-	echo "4.mul"
-	read -p "select menu : " temp
-		case $temp in
-		1)sum=$(("$num1" + "$num2"))
-			parameter=add;;
-		2)sum=$(("$num1" - "$num2"))
-                       parameter=sub;;
-	        3)sum=$(("$num1" / "$num2"))
-                        parameter=div;;
-		4)sum=$(("$num1" * "$num2"))
-                        parameter=mul;;
-	esac
-	
-	echo "num1 : $num1"
-	echo "num2 : $num2"
-	echo "op : $parameter"
-	echo "result : $sum"
-else 
-	 case $1 in
-		1)sum=$(("$num1" + "$num2"))
+        echo "...none operator paraeter..."
+        echo "1.add"
+        echo "2.sub"
+        echo "3.div"
+        echo "4.mul"
+        read -p "select menu : " temp
+                case $temp in
+                1)sum=$(("${arr[0]}" + "${arr[1]}"))
                         parameter=add;;
-		2)sum=$(("$num1" - "$num2"))
-                        parameter=sub;;
-		3)sum=$(("$num1" / "$num2"))
+                2)sum=$(("${arr[0]}" - "${arr[1]}"))
+                       parameter=sub;;
+                3)sum=$(("${arr[0]}" / "${arr[1]}"))
                         parameter=div;;
-		4)sum=$(("$num1" * "$num2"))
+                4)sum=$(("${arr[0]}" * "${arr[1]}"))
                         parameter=mul;;
         esac
 
-        echo "num1 : $num1"
-        echo "num2 : $num2"
+        echo "num1 : ${arr[0]}"
+        echo "num2 : ${arr[1]}"
+        echo "op : $parameter"
+        echo "result : $sum"
+else
+         case $1 in
+                1)sum=$(("${arr[1]}" + "${arr[1]}"))
+                        parameter=add;;
+                2)sum=$(("${arr[0]}" - "${arr[1]}"))
+                        parameter=sub;;
+                3)sum=$(("${arr[0]}" / "${arr[1]}"))
+                        parameter=div;;
+                4)sum=$(("${arr[0]}" * "${arr[1]}"))
+                        parameter=mul;;
+        esac
+
+        echo "num1 : ${arr[0]}"
+        echo "num2 : ${arr[1]}"
         echo "op : $parameter"
         echo "result : $sum"
 fi
 exit
 
-
-	
